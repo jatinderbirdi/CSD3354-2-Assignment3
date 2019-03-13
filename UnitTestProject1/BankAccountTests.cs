cf17b466d3c861e24d732aa13e6d9be9539b8913
visualstudio <visualstudio@122-PC15>
2019-03-13 10:46:10 -0400

    when ammount is more than zero

--- UnitTestProject1/BankAccountTests.cs
+++ UnitTestProject1/BankAccountTests.cs
@@ -24,20 +24,36 @@ namespace UnitTestProject1
             double actual = account.Balance;
             Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
         }
-        
+
         [TestMethod]
         [ExpectedException(typeof(ArgumentOutOfRangeException))]
         public void Debit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
         {
             //Arrange
             double beginningBalance = 11.99;
-            double debitAmount = -100.00;
+            double debitAmount = 100.00;
             BankAccount account = new BankAccount("Mr.Bryan Walton", beginningBalance);
 
             //Act
             account.Debit(debitAmount);
 
             //Assert is handled by the expectedException attribute on the test method.
+
+        }
+        [TestMethod]
+        
+        public void Debit_WhenAmountIsMoreThanZero_ShouldThrowArgumentOutOfRange()
+        {
+            //Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 100.00;
+            BankAccount account = new BankAccount("Mr.Bryan Walton", beginningBalance);
+
+            //Act
+            account.Debit(debitAmount);
+
+            //Assert is handled by the expectedException attribute on the test method.
+
         }
     }
 }
