6fecdc4378e26c92bbc0f076ee30232e4f471729
visualstudio <visualstudio@122-PC15>
2019-03-13 10:29:07 -0400

    CHECKPOINT-C

--- UnitTestProject1/BankAccountTests.cs
+++ UnitTestProject1/BankAccountTests.cs
@@ -23,6 +23,21 @@ namespace UnitTestProject1
             //Assert 
             double actual = account.Balance;
             Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
-        } 
+        }
+        
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Debit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
+        {
+            //Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = -100.00;
+            BankAccount account = new BankAccount("Mr.Bryan Walton", beginningBalance);
+
+            //Act
+            account.Debit(debitAmount);
+
+            //Assert is handled by the expectedException attribute on the test method.
+        }
     }
 }
